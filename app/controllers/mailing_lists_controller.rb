4fde8bc9e7c06dc4d0370d696a4e8bce8472c73b
Mike Griffin <mgriffin@github.com>
2024-05-03 12:28:44 +0100

    Set up thank you email when someone signs up to mailing list

--- app/controllers/mailing_lists_controller.rb
+++ app/controllers/mailing_lists_controller.rb
@@ -3,6 +3,7 @@ class MailingListsController < ApplicationController
     email = MailingList.new(mailing_list_params)
 
     if email.save
+      MailingListMailer.with(email: mailing_list_params[:address]).welcome_email.deliver_later
       redirect_to root_url, notice: "You've been added"
     else
       redirect_to root_url, error: "Couldn't add your email address"
---
+++ test/mailers/previews/mailing_list_mailer_preview.rb
@@ -0,0 +1,6 @@
+# Preview all emails at http://localhost:3000/rails/mailers/mailing_list_mailer
+class MailingListMailerPreview < ActionMailer::Preview
+  def welcome_email
+    MailingListMailer.with(email: User.first.email).welcome_email
+  end
+end
