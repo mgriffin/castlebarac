15d770f46dee9144eb74c8f074665a87df5641cb
Mike Griffin <mgriffin@github.com>
2024-05-05 12:40:54 +0100

    Add a banner styled on hacker news

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,7 @@ class SiteController < ApplicationController
   def index
     @mailing_list = MailingList.new
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :mailing_lists, only: :create
 
+  get "/about", to: "site#about", as: :about
   root "site#index"
 end
