7325498aba26c389533c2548d1e6b297d8f06673
Mike Griffin <mgriffin@github.com>
2024-05-22 10:14:01 +0100

    Send the original URL on errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,14 +2,14 @@
 
 class ErrorsController < ApplicationController
   def not_found
-    message = "#{request.method} #{request.url} wasn't found"
+    message = "#{request.method} #{request.original_url} wasn't found"
     PostToZulipJob.perform_later(message:, stream: "castlebarac", topic: "Page not found")
 
     render "errors/not_found", status: :not_found
   end
 
   def internal_error
-    message = "#{request.method} #{request.url} caused an error"
+    message = "#{request.method} #{request.original_url} caused an error"
     PostToZulipJob.perform_later(message:, stream: "castlebarac", topic: "Internal error")
 
     render "errors/internal_error", status: :internal_server_error
