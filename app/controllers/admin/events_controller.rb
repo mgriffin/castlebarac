4c568fafd0e5e3f7bcdb85d2d06855ecb06aa2c6
Mike Griffin <mgriffin@github.com>
2024-09-02 22:38:39 +0100

    Add and edit events as an admin

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -6,8 +6,42 @@ module Admin
 
     before_action :admin_only!
 
+    def show
+      @event = Event.find(params[:id])
+    end
+
     def new
       @event = Event.new
+    end
+
+    def edit
+      @event = Event.find(params[:id])
+    end
+
+    def create
+      @event = Event.new(event_params)
+
+      if @event.save
+        redirect_to admin_event_path(@event.id), notice: "Added #{@event.title}"
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+
+    def update
+      @event = Event.find(params[:id])
+
+      if @event.update(event_params)
+        redirect_to admin_event_path(@event.id), notice: "Added #{@event.title}"
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+    private
+
+    def event_params
+      params.require(:event).permit(:title, :start_time, :content)
     end
   end
 end
--- test/controllers/admin/events_controller_test.rb
+++ test/controllers/admin/events_controller_test.rb
@@ -25,5 +25,50 @@ module Admin
 
       assert_response :success
     end
+
+    test "anonymous user can't create new event" do
+      post admin_events_url, params: { event: { title: Faker::String.random, start_time: DateTime.now,  body: Faker::Markdown.sandwich } }
+
+      assert_response :not_found
+    end
+
+    test "logged in user can't create new event" do
+      sign_in :bugs
+
+      post admin_events_url, params: { event: { title: Faker::String.random, start_time: DateTime.now,  body: Faker::Markdown.sandwich } }
+
+      assert_response :not_found
+    end
+
+    test "admin user can create new event" do
+      sign_in :admin
+
+      post admin_events_url, params: { event: { title: Faker::String.random, start_time: DateTime.now,  body: Faker::Markdown.sandwich } }
+
+      assert_redirected_to admin_event_url(Event.last)
+    end
+
+    test "anonymous user can't edit a event" do
+      patch admin_event_url(Event.first.id), params: { event: { title: "edited the title" } }
+
+      assert_response :not_found
+    end
+
+    test "logged in user can't edit a event" do
+      sign_in :bugs
+
+      patch admin_event_url(Event.first.id), params: { event: { title: "edited the title" } }
+
+      assert_response :not_found
+    end
+
+    test "admin user can edit a event" do
+      sign_in :admin
+
+      patch admin_event_url(Event.first.id), params: { event: { title: "edited the title" } }
+
+      assert_redirected_to admin_event_url(Event.first)
+      assert_equal "edited the title", Event.first.title
+    end
   end
 end
