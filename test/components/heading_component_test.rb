bdfd9da75fedb0b30b599cff484243931f3d1be2
Mike Griffin <mgriffin@github.com>
2024-09-13 23:10:58 +0100

    Might as well have some tests too

--- test/components/heading_component_test.rb
+++ test/components/heading_component_test.rb
@@ -3,10 +3,16 @@
 require "test_helper"
 
 class HeadingComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(HeadingComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_component_renders_a_h2_heading_by_default
+    assert_equal(
+      %(<h2>Hello, components!</h2>),
+      render_inline(HeadingComponent.new(tag: :narf).with_content("Hello, components!")).css("h2").to_html
+    )
+  end
+  def test_component_renders_a_h1_heading
+    assert_equal(
+      %(<h1>Hello, components!</h1>),
+      render_inline(HeadingComponent.new(tag: :h1).with_content("Hello, components!")).css("h1").to_html
+    )
   end
 end
