1acf7ba751b459db206d0dc1321e8d548e8b8ddf
Mike Griffin <mgriffin@github.com>
2024-09-15 08:55:51 +0100

    Allow events to be shown

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -6,6 +6,7 @@ class SiteController < ApplicationController
   def index
     @mailing_list = MailingList.new
     @pagy, @posts = pagy(Post.order(created_at: :desc))
+    @events = Event.where(start_time: Time.now..).order(:start_time).limit(5)
   end
 
   def about; end
