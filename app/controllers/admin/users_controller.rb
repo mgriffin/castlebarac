55fa27241c41ec3999018223a8b81b62cc4ae089
Mike Griffin <mgriffin@github.com>
2024-11-28 15:24:00 +0000

    Toggle admin state with a checkbox

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,5 +9,23 @@ module Admin
     def index
       @pagy, @users = pagy(User.order(created_at: :desc))
     end
+
+    def update
+      @user = User.find(user_params[:id])
+
+      if @user.update_attribute(:admin, user_params[:admin])
+        respond_to do |format|
+          redirect_to admin_users_path
+        end
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:id, :admin)
+    end
   end
 end
