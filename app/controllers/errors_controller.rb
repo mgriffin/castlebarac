ac71a80c1f0e1a5e38d8438e1f273f87b6b5ce7b
Mike Griffin <mgriffin@github.com>
2024-12-10 00:44:29 +0000

    Styling and show backtrace

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -9,9 +9,12 @@ class ErrorsController < ApplicationController
   end
 
   def internal_error
+    exception = request.env["action_dispatch.exception"]
+    trace = ActionDispatch::ExceptionWrapper.new(Rails.backtrace_cleaner, exception).application_trace
+
     message = "#{request.method} #{request.original_url} caused an error"
     PostToZulipJob.perform_later(message:, stream: "castlebarac", topic: "Internal error")
 
-    render "errors/internal_error", status: :internal_server_error
+    render "errors/internal_error", status: :internal_server_error, locals: { trace: }
   end
 end
