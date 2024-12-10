160fbe4ab982932d1302e01d980b4cfa22e8e13a
Mike Griffin <mgriffin@github.com>
2024-12-10 00:54:36 +0000

    Include exception message

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -9,12 +9,14 @@ class ErrorsController < ApplicationController
   end
 
   def internal_error
-    exception = request.env["action_dispatch.exception"]
-    trace = ActionDispatch::ExceptionWrapper.new(Rails.backtrace_cleaner, exception).application_trace
+    exception = ActionDispatch::ExceptionWrapper.new(
+      Rails.backtrace_cleaner,
+      request.env["action_dispatch.exception"]
+    )
 
     message = "#{request.method} #{request.original_url} caused an error"
     PostToZulipJob.perform_later(message:, stream: "castlebarac", topic: "Internal error")
 
-    render "errors/internal_error", status: :internal_server_error, locals: { trace: }
+    render "errors/internal_error", status: :internal_server_error, locals: { exception: }
   end
 end
