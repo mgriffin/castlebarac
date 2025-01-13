410ae4f8f5b8b92f095c45e39b6877ab92af5a47
Mike Griffin <mgriffin@github.com>
2025-01-13 22:17:53 +0000

    Fix some more tests

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class EventsController < ApplicationController
-  allow_unauthenticated_access
-
   def index
     @future_events = Event.order(start_time: :asc).where(start_time: Time.zone.now...).group_by do |event|
       event.start_time.beginning_of_month
