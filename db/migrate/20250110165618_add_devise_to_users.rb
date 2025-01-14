c66c81b29e4214eb6aa1170cb7ee3ca17507a99a
Mike Griffin <mgriffin@github.com>
2025-01-14 09:34:35 +0000

    Ssh, migration changed

--- db/migrate/20250110165618_add_devise_to_users.rb
+++ db/migrate/20250110165618_add_devise_to_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AddDeviseToUsers < ActiveRecord::Migration[7.1]
-  def self.change
+  def self.up
     change_table :users do |t|
       ## Database authenticatable
       # t.string :email,              null: false, default: ""
@@ -40,5 +40,17 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.1]
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+  end
+
+  def self.down
+    change_table :users do |t|
+      t.rename :encrypted_password, :password_digest
+    end
+
+    remove_column :users, :reset_password_token, :string
+    remove_column :users, :reset_password_sent_at, :datetime
+    remove_column :users, :remember_created_at, :datetime
+
+    remove_index :users, :email,                unique: true
   end
 end
