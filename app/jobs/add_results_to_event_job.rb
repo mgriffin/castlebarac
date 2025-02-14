19a3a09d9e1cad95572130fc0b519c8472050337
Mike Griffin <mgriffin@github.com>
2025-02-14 12:24:29 +0000

    Next not return

--- app/jobs/add_results_to_event_job.rb
+++ app/jobs/add_results_to_event_job.rb
@@ -10,7 +10,7 @@ class AddResultsToEventJob < ApplicationJob
       race_name = r["name"].strip.gsub(" - Results", "")
       race = event.races.find_or_create_by(name: race_name)
 
-      return if r["participants"].nil?
+      next if r["participants"].nil?
 
       r["participants"].each do |p|
         race.results.find_or_create_by!(
