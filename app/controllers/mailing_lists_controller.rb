10fcbf94c92e2e171d8f869b7e1267d52f873ac8
Mike Griffin <mgriffin@github.com>
2025-03-12 22:52:41 +0000

    Remove Zulip notifications

--- app/controllers/mailing_lists_controller.rb
+++ app/controllers/mailing_lists_controller.rb
@@ -13,9 +13,6 @@ class MailingListsController < ApplicationController
     if email.save
       MailingListMailer.with(email: mailing_list_params[:address]).welcome.deliver_later
 
-      message = "#{mailing_list_params[:address]} has just signed up"
-      PostToZulipJob.perform_later(message:, stream: "general", topic: "New Mailing List Member")
-
       redirect_to root_url, notice: t(".notice")
     else
       redirect_to root_url, error: t(".error")
--- app/jobs/post_to_zulip_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class PostToZulipJob < ApplicationJob
-  queue_as :default
-
-  def perform(message:, stream:, topic:)
-    Zulip.new.post(message:, stream:, topic:)
-  end
-end
