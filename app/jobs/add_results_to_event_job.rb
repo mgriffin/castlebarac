d3831ea4683ae4b8f930f39fd1de2e9f157adb47
Mike Griffin <mgriffin@github.com>
2025-03-15 11:42:06 +0000

    Reduce the complexity a bit

--- app/jobs/add_results_to_event_job.rb
+++ app/jobs/add_results_to_event_job.rb
@@ -4,11 +4,6 @@ class AddResultsToEventJob < ApplicationJob
   queue_as :default
 
   def perform(event:, url:)
-    result_ids = Nokogiri::HTML(Typhoeus.get("#{url}/competition.php").body)
-                         .css(".schedule-table tr")
-                         .collect { |tr| tr.attribute("onclick")&.value }
-                         .compact.map { |text| text.match(/results\.php\?id=(\d+)/).captures.first }
-
     result_ids.each do |id|
       doc = Nokogiri::HTML(Typhoeus.get("#{url}/results.php", params: { id: id }).body)
 
@@ -29,30 +24,33 @@ class AddResultsToEventJob < ApplicationJob
       ### Don't create anything if there's no results
       next if results["1"]["allParticipants"].nil?
 
-      race_name = results["1"]["name"].strip.gsub(" - Results", "")
+      race = event.races.find_or_create_by(name: name)
 
-      ### In the JSON, the race name is often not included, and
-      ### instead there's an asterisk. If that happens, change it
-      race_name = name if race_name == "*"
-      race = event.races.find_or_create_by(name: race_name)
+      create_results(race, results["1"]["allParticipants"])
+    end
+  end
 
-      results["1"]["allParticipants"].each do |p|
-        name = if p["isRelay"]
-                 p["teamName"].strip
-               else
-                 p["name"].strip
-               end
-        race.results.find_or_create_by!(
-          name: name,
-          club: p["club"],
-          result: p["result"],
-          seconds: p["sortValue"],
-          heat: p["heatNumber"],
-          bib: p["bib"],
-          category: p["categoryAbbr"],
-          position: p["displayPosition"]
-        )
-      end
+  def result_ids
+    Nokogiri::HTML(Typhoeus.get("#{url}/competition.php").body)
+            .css(".schedule-table tr")
+            .collect { |tr| tr.attribute("onclick")&.value }
+            .compact.map { |text| text.match(/results\.php\?id=(\d+)/).captures.first }
+  end
+
+  def create_results(race, participants)
+    participants.each do |p|
+      name = p["isRelay"] ? p["teamName"].strip : p["name"].strip
+
+      race.results.find_or_create_by!(
+        name: name,
+        club: p["club"],
+        result: p["result"],
+        seconds: p["sortValue"],
+        heat: p["heatNumber"],
+        bib: p["bib"],
+        category: p["categoryAbbr"],
+        position: p["displayPosition"]
+      )
     end
   end
 end
