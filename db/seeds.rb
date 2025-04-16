61a51841def820d32a985c8111a4f2103f166083
Mike Griffin <mgriffin@github.com>
2025-04-16 22:21:26 +0100

    Seed some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.find_or_create_by(email: "bugs@acme.fake") do |u|
+  u.password = "carrots"
+  u.password_confirmation = "carrots"
+end
+
+Team.find_or_create_by(name: "Admins") do |t|
+  t.slug = "admins"
+end
