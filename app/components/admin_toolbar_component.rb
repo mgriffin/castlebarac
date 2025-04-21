11dfcd349ce67c17a3989c9462ba09ea3d906155
Mike Griffin <mgriffin@github.com>
2025-04-21 22:30:18 +0100

    Make component tests work

--- app/components/admin_toolbar_component.rb
+++ app/components/admin_toolbar_component.rb
@@ -4,7 +4,14 @@ class AdminToolbarComponent < ViewComponent::Base
   include Pundit::Authorization
   include Devise::Controllers::Helpers
 
+  def initialize(user:)
+    @current_user = user
+    super
+  end
+
   def render?
+    return false if @current_user.nil?
+
     policy(:admin_toolbar).show?
   end
 end
