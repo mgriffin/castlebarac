eec9e41c9015cfa3e3b175abdddf4005074ecbd0
Mike Griffin <mgriffin@github.com>
2025-06-08 23:18:14 +0100

    Have a nice conversion from distance to human form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def distance_to_human(distance)
+    case distance
+    when 0..4999
+      "#{distance}m"
+    else
+      "#{distance/1000}km"
+    end
+  end
 end
---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ApplicationHelperTest < ActionView::TestCase
+  [
+    [ 60, "60m" ],
+    [ 100, "100m" ],
+    [ 5000, "5km" ],
+    [ 10000, "10km" ],
+  ].each do |input, expected|
+    test "distance_to_human for #{input} metres" do
+      actual = distance_to_human(input)
+      assert_dom_equal expected, actual, "Expected #{input} metres to be formatted as #{expected}, but it was #{actual}"
+    end
+  end
+end
