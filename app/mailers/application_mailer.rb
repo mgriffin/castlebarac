26e91ccbdd87196d8868d2441ee8d6c2de043565
Mike Griffin <mgriffin@github.com>
2025-06-23 23:30:21 +0100

    Lets try this header again

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
+  after_action set_postmark_header
+
   default from: "info@castlebarac.ie"
-  headers "X-PM-Message-Stream": "outbound"
   layout "mailer"
+
+  private
+
+  def set_postmark_header
+    headers["X-PM-Message-Stream"] = "outbound"
+  end
 end
