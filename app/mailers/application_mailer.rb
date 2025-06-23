29455fb2545c82a2ce76b5328f96a40b0b17be96
Mike Griffin <mgriffin@github.com>
2025-06-23 23:32:51 +0100

    Missed a colon

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  after_action set_postmark_header
+  after_action :set_postmark_header
 
   default from: "info@castlebarac.ie"
   layout "mailer"
