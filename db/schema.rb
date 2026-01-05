65de6a95ce68408317c9f1e48c5e57e367e58307
Mike Griffin <github@mikegriffin.ie>
2026-01-05 12:16:30 +0000

    Add thredded database migrations
    Also added a username field to the Users table so that @mentions will
    work. This field will have to be populated for all the production users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_13_131252) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_05_110321) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -465,6 +465,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_13_131252) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
