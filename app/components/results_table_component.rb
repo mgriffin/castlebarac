82b3d68692bd19604acb1b622edb31c81f2a979c
Mike Griffin <github@mikegriffin.ie>
2026-02-11 09:09:26 +0000

    Move race results into components to speed things up

---
+++ app/components/results_table_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class ResultsTableComponent < ViewComponent::Base
+  renders_many :results, ResultComponent
+
+  def initialize(first_result:)
+    @first_result = first_result
+  end
+
+  def headers
+      class_names({
+        "position" => true,
+        "bib" => @first_result&.bib.present?,
+        "name" => true,
+        "club" => true,
+        "county" => @first_result&.county.present?,
+        "time" => @first_result&.seconds.nil?
+      }).split
+  end
+end
